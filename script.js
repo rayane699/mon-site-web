6a432b545313e59bea3861fabc460b259414b010
merdji <rayanemerdji.lyon@gmail.com>
2024-06-30 21:38:28 +0200

    Ajout des fichiers de mon site web

---
+++ script.js
@@ -0,0 +1,17 @@
+// Ajouter un écouteur d'événement pour le bouton avec l'ID 'btnRedirectDocker'
+document.getElementById('btnRedirectDocker').addEventListener('click', function() {
+    // Lorsque le bouton est cliqué, rediriger vers le site de Docker
+    window.location.href = 'https://www.docker.com';
+});
+
+// Ajouter un écouteur d'événement pour le bouton avec l'ID 'btnRedirectVSCode'
+document.getElementById('btnRedirectVSCode').addEventListener('click', function() {
+    // Lorsque le bouton est cliqué, rediriger vers le site de Visual Studio Code
+    window.location.href = 'https://code.visualstudio.com/';
+});
+
+// Ajouter un écouteur d'événement pour le bouton avec l'ID 'btnRedirectCanva'
+document.getElementById('btnRedirectCanva').addEventListener('click', function() {
+    // Lorsque le bouton est cliqué, rediriger vers le site de canva
+    window.location.href = 'https://www.canva.com//';
+});
